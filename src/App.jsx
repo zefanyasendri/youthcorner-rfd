9b913354d3a4d4b98d6d3059a109a6285df12178
Zefanya Sendri <zefanyasendri@gmail.com>
2023-08-05 16:24:22 +0700

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+import './assets/css/app.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <h1 className="text-xs font-bold underline">
-        Hello dd!
+      <h1 className="text-3xl font-bold underline">
+        Hero
       </h1>
     </>
   )
