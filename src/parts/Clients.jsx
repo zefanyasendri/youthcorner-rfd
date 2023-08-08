eb369b09d72708d51235e5fc5ff0cd70e9943790
Zefanya Sendri <zefanyasendri@gmail.com>
2023-08-09 01:23:24 +0700

    Add Logo

--- src/parts/Clients.jsx
+++ src/parts/Clients.jsx
@@ -8,12 +8,22 @@ export default function Clients() {
                     <div
                     className="w-full flex-auto md:w-auto md:flex-initial px-4 md:px-6 my-4 md:my-0"
                     >
-                        <img src="/images/content/logo-gp.svg" alt="" className="mx-auto" />
+                        <img src="/images/content/logo-react.svg" alt="" className="mx-auto" />
                     </div>
                     <div
                     className="w-full flex-auto md:w-auto md:flex-initial px-4 md:px-6 my-4 md:my-0"
                     >
-                        <img src="/images/content/logo-gpib.svg" alt="" className="mx-auto" />
+                        <img src="/images/content/logo-figma.svg" alt="" className="mx-auto" />
+                    </div>
+                    <div
+                    className="w-full flex-auto md:w-auto md:flex-initial px-4 md:px-6 my-4 md:my-0"
+                    >
+                        <img src="/images/content/logo-postman.svg" alt="" className="mx-auto" />
+                    </div>
+                    <div
+                    className="w-full flex-auto md:w-auto md:flex-initial px-4 md:px-6 my-4 md:my-0"
+                    >
+                        <img src="/images/content/logo-bwa.svg" alt="" className="mx-auto" />
                     </div>
                 </div>
             </section>
