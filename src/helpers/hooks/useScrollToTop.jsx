4bc8d387f94640e2f0bfd73c7fdcc64f38750124
Zefanya Sendri <zefanyasendri@gmail.com>
2023-08-09 00:35:06 +0700

    Update Minor Detail

--- src/helpers/hooks/useScrollToTop.jsx
+++ src/helpers/hooks/useScrollToTop.jsx
@@ -1,12 +1,12 @@
 import {useLayoutEffect} from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
 
 export default function useScrollToTop() {
-    const navigate = useNavigate()
+    const location = useLocation()
     useLayoutEffect(() => {
         window.scrollTo({
             top: 0, left: 0, behavior: 'smooth'
         })
-    }, [navigate.location?.key])
+    }, [location.key])
 }
